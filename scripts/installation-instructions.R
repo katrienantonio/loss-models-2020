b1faae306384c542ef88a1481b8fb3d1e1d43365
Katrien Antonio <u0043788@kuleuven.be>
2020-12-14 19:55:59 +0100

    add installation instructions

---
+++ scripts/installation-instructions.R
@@ -0,0 +1,10 @@
+packages <- c("tidyverse", "here", "gridExtra", "grid", "rstudioapi", "MASS", "actuar", "statmod", "ReIns", "pscl")
+new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+if(sum(!(packages %in% installed.packages()[, "Package"]))) {
+  stop(paste('The following required packages are not installed:\n', 
+             paste(packages[which(!(packages %in% installed.packages()[, "Package"]))], collapse = ', ')));
+} else {
+  message("Everything is set up correctly. You are ready to go.")
+}
